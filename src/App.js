1e9f5c3f3c932f25a6bac6f96e9e8ed931006d96
tricknp <np_patrick@yahoo.com>
2018-06-04 19:43:28 -0300

    add db & basic layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import axios from 'axios'
 class App extends Component {
+
+  componentWillMount(){
+    axios
+      .get('http://localhost:3001/genres')
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log('deu ruim')
+      })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div className="container">
+              <div className="navbar-header page-scroll">
+                <a className="navbar-brand page-scroll" href="#page-top">
+                    <img src="images/logo.png" height="30" />
+                </a>
+              </div>
+
+              <div className="collapse navbar-collapse navbar-ex1-collapse">
+                <ul className="nav navbar-nav">
+                  <li>
+                    <a href="">Menu item</a>
+                  </li>
+                </ul>
+              </div>
+
+            </div>
+          </nav>
+
+
+          <section id="intro" className="intro-section">
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-12">
+                  <h1><img src="images/logo.png" /></h1>
+                  <p>Nunca mais esqueça uma série que você assistiu ou que alguém lhe indicou.</p>
+                </div>
+              </div>
+            </div>
+          </section>        
       </div>
     );
   }
